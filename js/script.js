5317453b6a24e53166a333577092bead0775c7f7
Arthur Victor <arthurvictor1404@gmail.com>
2025-10-15 18:03:36 -0300

    fead:desenvolvimento da logica

--- js/script.js
+++ js/script.js
@@ -1,43 +1,69 @@
-const menu = document.getElementById("menu")
-const cartBtn = document.getElementById("cart-btn")
-const cartModal = document.getElementById("modal")
-const cartItemsContainer = document.getElementById("cart-items")
-const cartTotal = document.getElementById("cart-total")
-const checkoutBtn = document.getElementById("finish-btn")
-const closeBtn = document.getElementById("close")
-const cartCounter = document.getElementById("cart-count")
-const alertInput = document.getElementById("address")
-const alertWarn = document.getElementById("address-warn")
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartCount = document.getElementById("cart-count");
+const cartModal = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const finishBtn = document.getElementById("finish-btn");
+const alertInput = document.getElementById("address");
+const alertWarn = document.getElementById("address-warn");
 
 let cart = [];
 
-// abrir modal
-cartBtn.addEventListener("click", function() {
-    cartModal.style.display = "flex"
-})
+cartBtn.addEventListener("click", function () {
+  updateCartModal();
+  cartModal.style.display = "flex";
+});
 
-// fechar modal clicando fora
-cartModal.addEventListener("click", function(event) {
-    if(event.target === cartModal) {
-        cartModal.style.display = "none"
-    }
-})
-
-// btn-close
 closeBtn.addEventListener("click", function () {
-    cartModal.style.display = "none"
-})
+  cartModal.style.display = "none";
+});
 
-// adicionar no carrinho
-menu.addEventListener("click", function(event) {
-    let parentButton = event.target.closest(".cart-btn")
-    if(parentButton) {
-        const name = parentButton.getAttribute("data-name")
-        const price = parseFloat(parentButton.getAttribute("data-price"))
-    }
-})
+menu.addEventListener("click", function (event) {
+  let parentButton = event.target.closest(".addCart-btn");
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parentButton.getAttribute("data-price");
+    addToCart(name, price);
+  }
+});
 
-// finção add cart
 function addToCart(name, price) {
-    cart.push({name, price, quantity: 1,})
+  const existingItem = cart.find((item) => item.name === name);
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({ name, price, quantity: 1 });
+  }
+  updateCartModal();
+}
+
+function updateCartModal() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+  cart.forEach((item) => {
+    const cartItemsElement = document.createElement("div");
+    cartItemsElement.style.display = "flex";
+    cartItemsElement.style.justifyContent = "space-between";
+    cartItemsElement.style.flexDirection = "column";
+    
+    cartItemsElement.innerHTML = `
+        <div style = "display: flex; align-items: center; justify-content: space-between;">
+            <div>
+                <p style = "font-size: bold;">${item.name}</p>
+                <p style = "font-size: medium;">Qtd: ${item.quantity}</p>
+                <p style = "margin-top: 16px;">R$ ${item.price}</p>
+            </div>
+
+            <div>
+                <button style = "border: none; background: #ffffff; cursor: pointer;">
+                Remover
+                </button>
+            </div>
+        </div>
+        `;
+
+    cartItemsContainer.appendChild(cartItemsElement);
+  });
 }
